dc6fd54a794551c476dfa5055b63214058c0bc7f
duonght15 <htd19051995@gmail.com>
2023-05-23 13:59:08 +0700

    Create Movement.cs

---
+++ Movement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 100;
+    [SerializeField] float rotationThrust = 1f;
+    Vector3 force;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(mainThrust*Vector3.up*Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // Freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation= false; // Unfreezing rotation so physics system can take over
+    }
+
+    
+
+    
+    
+}
